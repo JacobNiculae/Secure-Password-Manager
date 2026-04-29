e044f0b56e24da875299ad81091fac64418a342b
JacobNiculae04 <janiculae@csu.fullerton.edu>
2026-04-29 00:14:30 -0700

    fixed callback error preventing main window from appearing

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from ui.main_window import MainWindow
 from server.local_server import run_server
 from db.database import init_db
 
+main_window = None
+
 def start_server():
     thread = threading.Thread(target=run_server, daemon=True)
     thread.start()
 
 def main():
+    global main_window
     init_db()
     start_server()
 
     app = QApplication(sys.argv)
 
     def on_login_success(key: bytes):
+        global main_window
         main_window = MainWindow(key)
         main_window.show()
 
